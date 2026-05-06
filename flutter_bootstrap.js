ca1fb86aa30c82b1f89bfa36c62cace056a15b75
NathanDaud123 <NathanDaud123@users.noreply.github.com>
2026-05-06 18:49:06 +0000

    deploy: d23b1fb7865c48f505f0b1f57c8e0cda8150ac30

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1189341354"
+    serviceWorkerVersion: "2515267445"
   }
 });
